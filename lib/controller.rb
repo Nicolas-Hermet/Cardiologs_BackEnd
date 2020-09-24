aec7a21b0f503edeb308b5033f13c7f4b4f141dd
Nicolas Hermet <n.hermet@gmail.com>
2020-09-24 20:47:31 +0200

    feat(routes): add root and delineation routes

--- lib/controller.rb
+++ lib/controller.rb
@@ -4,4 +4,11 @@ require 'sinatra'
 
 # Delineation App
 class ApplicationController < Sinatra::Base
+  get '/' do
+    redirect '/delineation/new'
+  end
+
+  get '/delineation/new' do
+    'Hello World'
+  end
 end
