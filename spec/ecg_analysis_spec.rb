465fa4fce12006f954642bac8da7215e7db7c789
Nicolas Hermet <n.hermet@gmail.com>
2020-09-26 17:27:33 +0200

    test(rate computations): test whether rates are correctly computed

--- spec/ecg_analysis_spec.rb
+++ spec/ecg_analysis_spec.rb
@@ -15,5 +15,8 @@ describe 'ecg analysis class' do
     expect(ecg.raw_data).to eq(data)
     expect(ecg.premature_p_wave_number).to eq(2)
     expect(ecg.premature_qrs_wave_number).to eq(1)
+    expect(ecg.max_rate).to eq([50.8, [17976, 19158]])
+    expect(ecg.min_rate).to eq([43.0, [6571]])
+    expect(ecg.mean_rate).to eq(48.1)
   end
 end
