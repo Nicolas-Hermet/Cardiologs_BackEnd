ccfeae0d1befa1526823813a442e53422da38bf6
Nicolas Hermet <n.hermet@gmail.com>
2020-09-26 11:07:15 +0200

    test(analysis): check if data are correctly parsed

---
+++ spec/ecg_analysis_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'ecg_analysis'
+require 'read_csv'
+
+describe 'ecg analysis class' do
+  data = ReadFile.extract_data('./spec/input_test.csv')
+
+  it 'has same raw data as the file' do
+    expect { ECGAnalysis.new(data) }.not_to raise_error
+  end
+
+  ecg = ECGAnalysis.new(data)
+  it 'parses data' do
+    expect(ecg.raw_data).to eq(data)
+    expect(ecg.premature_p_wave_number).to eq(2)
+    expect(ecg.premature_qrs_wave_number).to eq(1)
+  end
+end
